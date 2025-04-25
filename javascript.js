5af0e2e5c9b4ddea7df4889527480883b4f88d3c
Muthulakshmipython <muthulakshmipython@vetritechnologysolutions.in>
2025-04-26 00:33:08 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -89,6 +89,14 @@ function goToHome() {
   return false; 
 }
 
-// contact
+
+const currentPage = location.pathname.split("/").pop();
+
+  document.querySelectorAll(".nav-link").forEach(link => {
+    const linkPage = link.getAttribute("href").split("/").pop();
+    if (linkPage === currentPage) {
+      link.classList.add("active");
+    }
+  });
 
 
